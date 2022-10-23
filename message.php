b1565c58e4a87aa8c0c09d5da66abfd79fec38dc
MEZ <marczamora143@gmail.com>
2022-10-24 03:11:16 +0800

    v4
    complete change of chatbot and databases

---
+++ message.php
@@ -0,0 +1,32 @@
+<?php
+session_start();
+// Change this to your connection info.
+$DATABASE_HOST = 'localhost';
+$DATABASE_USER = 'root';
+$DATABASE_PASS = '';
+$DATABASE_NAME = 'ramit';
+// Try and connect using the info above.
+$con = mysqli_connect($DATABASE_HOST, $DATABASE_USER, $DATABASE_PASS, $DATABASE_NAME);
+if ( mysqli_connect_errno() ) {
+	// If there is an error with the connection, stop the script and display the error.
+	exit('Failed to connect to MySQL: ' . mysqli_connect_error());
+}
+// getting user message through ajax
+$getMesg = mysqli_real_escape_string($con, $_POST['text']);
+
+//checking user query to database query
+$check_data = "SELECT replies FROM cbr WHERE queries LIKE '%$getMesg%'";
+$run_query = mysqli_query($con, $check_data) or die("Error");
+
+// if user query matched to database query we'll show the reply otherwise it go to else statement
+if(mysqli_num_rows($run_query) > 0){
+    //fetching replay from the database according to the user query
+    $fetch_data = mysqli_fetch_assoc($run_query);
+    //storing replay to a varible which we'll send to ajax
+    $replay = $fetch_data['replies'];
+    echo $replay;
+}else{
+    echo "Sorry can't be able to understand you!";
+}
+
+?>
--- profile.php
+++ profile.php
@@ -9,17 +9,17 @@ if (!isset($_SESSION['loggedin'])) {
 $DATABASE_HOST = 'localhost';
 $DATABASE_USER = 'root';
 $DATABASE_PASS = '';
-$DATABASE_NAME = 'test';
+$DATABASE_NAME = 'ramit';
 $con = mysqli_connect($DATABASE_HOST, $DATABASE_USER, $DATABASE_PASS, $DATABASE_NAME);
 if (mysqli_connect_errno()) {
     exit('Failed to connect to MySQL: ' . mysqli_connect_error());
 }
 // We don't have the password or email info stored in sessions so instead we can get the results from the database.
-$stmt = $con->prepare('SELECT password, email FROM accounts WHERE id = ?');
+$stmt = $con->prepare('SELECT pswd, email, fname, mname, lname, pstion FROM accounts WHERE id = ?');
 // In this case we can use the account ID to get the account info.
 $stmt->bind_param('i', $_SESSION['id']);
 $stmt->execute();
-$stmt->bind_result($password, $email);
+$stmt->bind_result($pswd, $email, $fname, $mname, $lname, $pstion);
 $stmt->fetch();
 $stmt->close();
 ?>
@@ -29,35 +29,56 @@ $stmt->close();
 <head>
 <meta charset="utf-8">
 <title>Profile Page</title>
-<link href="code/css/style.css" rel="stylesheet" type="text/css">
+<link rel="stylesheet" href="code/css/cbcollapse.css">
+<link href="code/css/home.css" rel="stylesheet" type="text/css">
 </head>
 <body class="loggedin">  
-<nav class="navtop">
-<div>
-<h1>Website Title</h1>
-<a href="profile.html">Profile</a>
-<a href="logout.php">Logout</a>
-</div>
-</nav>
-<div class="content">
-<h2>Profile Page</h2>
-<div>
-<p>Your account details are below:</p>
-<table>
-<tr>
-<td>Username:</td>
-<td><?=$_SESSION['name']?></td>
-</tr>
-<tr>
-<td>Password:</td>
-<td><?=$password?></td>
-</tr>
-<tr>
-<td>Email:</td>
-<td><?=$email?></td>
-</tr>
-</table>
-</div>
-</div>
+    <nav class="navtop">
+        <div>
+            <h1><a href="home.php">RAM-IT</a></h1>
+            <a href="profile.php">Profile</a>
+            <a href="logout.php">Logout</a>
+        </div>
+    </nav>
+    <div class="content">
+        <h2>Profile Page</h2>
+        <div>
+            <p>Your account details are below:</p>
+            <!-- info table -->
+            <table>
+                <tr>
+                    <td>Name:</td>
+                    <td><?=$_SESSION['fname']?> <?=$_SESSION['mname']?> <?=$_SESSION['lname']?></td>
+                </tr>
+                <tr>
+                    <td>Password:</td>
+                    <td><?=$pswd?></td>
+                </tr>
+                <tr>
+                    <td>Email:</td>
+                    <td><?=$email?></td>
+                </tr>
+                <tr>
+                    <td>ID no#:</td>
+                    <td><?=$_SESSION['id']?></td>
+                </tr>
+                <tr>
+                    <td>Position:</td>
+                    <td><?=$pstion?></td>
+                </tr>
+            </table>
+        </div>
+    </div>
+<!-- chatbot -->
+    <button class="open-button" onclick="openForm()">Chatbot</button>
+        <div class="chat-popup" id="myForm">
+            <div class="form-container">
+                <?php include "cb.php"?>
+    <button type="button"  class="btn cancel" onclick="closeForm()">Close</button>
+            </div>
+        </div>
 </body>
+    <script src="code/scripts/cbcollapse.js"></script>
+	<script src="code/scripts/cb.js"></script>
+	<script src="https://kit.fontawesome.com/a076d05399.js"></script>
 </html>
